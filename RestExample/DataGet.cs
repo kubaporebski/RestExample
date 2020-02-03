78c88e0a05f40e2b37ab392ae7e4511a4987c2de
Porębski Jakub <jakub.porebski@microsoft.wsei.edu.pl>
2020-02-03 20:05:57 +0100

    First commit with files. Let's go!

---
+++ RestExample/DataGet.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestExample
+{
+    public class BDL
+    {
+        [SqlFunction]
+        public static SqlString GetTopSubjects(string apiKey)
+        {
+            string url = "https://bdl.stat.gov.pl/api/v1/subjects?format=json";
+
+            using (var client = new WebClient())
+            {
+                client.Headers["X-ClientId"] = apiKey;
+                return new SqlString(client.DownloadString(url));
+            }
+        }
+    }
+}
