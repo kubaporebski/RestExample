9d267fe65c46e8926742476bc854122bf75cbc59
Porębski Jakub <jakub.porebski@microsoft.wsei.edu.pl>
2020-02-04 20:56:22 +0100

    Ongoing work
    - added test unit project
    - class project is probably in required shape
    - still beta version

--- RestExample/DataGet.cs
+++ RestExample/DataGet.cs
@@ -1,29 +1,46 @@
 ﻿using Microsoft.SqlServer.Server;
-using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Data.SqlTypes;
 using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace RestExample
 {
     public class BDL
     {
-        [SqlFunction]
-        public static SqlString FetchTopSubjects(string apiKey)
+        [SqlFunction(FillRowMethodName = "FetchDataFillRow", TableDefinition = "Id int, Year int, Value float")]
+        public static IEnumerable FetchData(string apiKey, int variableId, string unitParentId, int yearFrom, int yearTo, int level)
         {
-            string url = "https://bdl.stat.gov.pl/api/v1/subjects?format=json&page-size=100";
-
-            using (var client = new WebClient())
+            string url = $"https://bdl.stat.gov.pl/api/v1/data/by-variable/{variableId}?format=xml&";
+            url += string.Join("&", Enumerable.Range(yearFrom, yearTo - yearFrom + 1).Select(yr => "year=" + yr));
+            url += "&page-size=100";
+            url += $"&unit-level={level}";
+            url += unitParentId != null ? $"&unit-parent-id={unitParentId}" : "";
+            
+            using (WebClient client = new WebClient())
             {
                 client.Headers["X-ClientId"] = apiKey;
-
-                var utf8string = Encoding.UTF8.GetString(client.DownloadData(url));
-                return new SqlString(utf8string);
+                
+                XDocument doc = XDocument.Parse(Encoding.UTF8.GetString(client.DownloadData(url)));
+                return doc.Descendants("yearVal").Select((yearVal, id) => new Item()
+                {
+                    Id = id + 1,
+                    Year = int.Parse(yearVal.Element("year").Value),
+                    Value = double.Parse(yearVal.Element("val").Value)
+                });
             }
         }
+
+        public static void FetchDataFillRow(object ob, out int Id, out int Year, out double Value)
+        {
+            var row = ob as Item;
+            Id = row.Id;
+            Year = row.Year;
+            Value = row.Value;
+        }
+
 
         [SqlFunction]
         public static SqlString FetchMeasureUnits(string apiKey)
@@ -39,4 +56,15 @@ namespace RestExample
             }
         }
     }
+
+    public class Item
+    {
+        public int Id { get; set; }
+
+        public int Year { get; set; }
+
+        public double Value { get; set; }
+
+    }
+
 }
---
+++ UnitTestProject/UnitTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestExample;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            IEnumerable result = BDL.FetchData("e595b260-5874-48e6-d0a1-08d6d44dd662", 2137, "010000000000", 2015, 2018, 1);
+            Assert.IsNotNull(result);
+
+            foreach (Item item in result)
+            {
+                Assert.IsNotNull(item);
+                Assert.IsTrue(item.Year > 0);
+            }
+            
+        }
+    }
+}
